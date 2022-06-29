6000d8378c10460c631ffb347fa6468227a70ee3
13Colours <tasker.prabhat@gmail.com>
2022-06-29 21:50:39 +1000

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require('vite')
 module.exports = defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
+      entry: path.resolve(__dirname, 'main.js'),
       name: 'myModule',
       fileName: format => `my-module.${format}.js`,
     },
